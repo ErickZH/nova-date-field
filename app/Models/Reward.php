aea5af106e259f9d71ed8a1678fcc810446f22cd
Erick Zarate Hernandez <zaraterick@outlook.com>
2021-09-06 20:58:14 -0500

    Init commit

---
+++ app/Models/Reward.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reward extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var string[]|bool
+     */
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'valid_to' => 'date',
+    ];
+}
